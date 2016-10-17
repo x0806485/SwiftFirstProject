de5170ab62007ac6f9d78c3c168cc802fb0beac4
zjc <x0806485@163.com>
2016-10-17 15:26:24 +0800

    first project

---
+++ SwiftFirstProject/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  SwiftFirstProject
+//
+//  Created by baihemiyu001 on 16/10/17.
+//  Copyright © 2016年 zjc. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
